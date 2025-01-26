4fdbfd81f3d6806244c775bb0c721b52aa37d545
CyrilBaah <baah_yard12@yahoo.com>
2025-01-26 21:25:37 +0000

    COnfigure database

---
+++ empuje/database.py
@@ -0,0 +1,22 @@
+import configparser
+from pathlib import Path
+
+from empuje import DB_WRITE_ERROR, SUCCESS
+
+DEFAULT_DB_FILE_PATH = Path.home().joinpath("." + Path.home().stem + "_empuje.json")
+
+
+def get_database_path(config_file: Path) -> Path:
+    """Return the current path to the empuje database"""
+    config_parser = configparser.ConfigParser()
+    config_parser.read(config_file)
+    return Path(config_parser["General"]["database"])
+
+
+def init_database(db_path: Path) -> int:
+    """Create the empuje database"""
+    try:
+        db_path.write_text("[]")
+        return SUCCESS
+    except OSError:
+        return DB_WRITE_ERROR
